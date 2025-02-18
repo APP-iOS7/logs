194013eaccb40a9e8aa6a6c604fdfc8c17c53ab9
JM Bae <jungman82@gmail.com>
2025-02-18 11:36:25 +0900

    StreamBuilder 예제를 위한 새로운 파일 추가

---
+++ 05-Dart_and_Flutter/13-data-json/my_data_demo/lib/6_stream_builder.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyWidget());
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('StreamBuilder 예제')),
+      body: Center(child: Text('StreamBuilder 예제')),
+    );
+  }
+}
