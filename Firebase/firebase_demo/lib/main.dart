390eafeb625757eabd8bfcd08d225b66e5c1c3b2
JM Bae <jungman82@gmail.com>
2025-02-21 10:08:56 +0900

    디버그 모드에서만 Firebase Firestore 에뮬레이터 사용하도록 변경

--- Firebase/firebase_demo/lib/main.dart
+++ Firebase/firebase_demo/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,8 +7,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  // Firebase Firestore Emulator 사용
-  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  // 디버그 모드일 때만 에뮬레이터 사용
+  if (kDebugMode) {
+    // Firebase Firestore Emulator 사용
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
 
   runApp(const MyApp());
 }
