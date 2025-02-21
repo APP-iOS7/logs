186a48da58a935d96d233958ae49504c20af8d43
JM Bae <jungman82@gmail.com>
2025-02-21 10:25:50 +0900

    Firebase 인증 화면 UI 추가 및 메인 페이지 변경

--- Firebase/firebase_demo/lib/main.dart
+++ Firebase/firebase_demo/lib/main.dart
@@ -30,7 +30,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AuthScreen(),
+    );
+  }
+}
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Firebase Auth Sign In')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: const Text('로그인')),
+            ElevatedButton(onPressed: () {}, child: const Text('회원가입')),
+          ],
+        ),
+      ),
     );
   }
 }
