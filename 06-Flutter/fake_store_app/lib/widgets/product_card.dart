f3ac2d4aaf47109c5e6b9b6ee56eae37d5932cc0
JM Bae <jungman82@gmail.com>
2025-02-26 13:12:06 +0900

    상품 상세 화면 및 목록 화면 추가: 제품 상세 조회 기능 구현 product_list_screen 이름 오류 수정

--- 06-Flutter/fake_store_app/lib/widgets/product_card.dart
+++ 06-Flutter/fake_store_app/lib/widgets/product_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/product.dart';
+import '../views/product_detail_screen.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -11,15 +12,25 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(8),
-      child: ListTile(
-        leading: Image.network(
-          product.image,
-          width: 50,
-          height: 50,
-          fit: BoxFit.cover,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProductDetailScreen(productId: product.id),
+            ),
+          );
+        },
+        child: ListTile(
+          leading: Image.network(
+            product.image,
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+          ),
+          title: Text(product.title),
+          subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
         ),
-        title: Text(product.title),
-        subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
       ),
     );
   }
