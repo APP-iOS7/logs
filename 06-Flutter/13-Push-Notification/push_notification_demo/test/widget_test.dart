6a493ca12832c4122a60f057cb793c555860c11f
JM Bae <jungman82@gmail.com>
2025-03-05 15:17:19 +0900

    푸시 알림 UI 테스트 추가: 테스트 케이스 이름 변경 및 알림 권한 요청 텍스트 검증

--- 06-Flutter/13-Push-Notification/push_notification_demo/test/widget_test.dart
+++ 06-Flutter/13-Push-Notification/push_notification_demo/test/widget_test.dart
@@ -5,26 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:push_notification_demo/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Push Notification Demo UI 테스트', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('알림 권한 요청'), findsOneWidget);
+    expect(find.text('알림 테스트'), findsOneWidget);
   });
 }
