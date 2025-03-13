540df0d9f9e0b3aaf5c709cfb5204266c9c0a3bf
JM Bae <jungman82@gmail.com>
2025-03-13 09:57:12 +0900

    * 관련 메서드를 새로운 파일로 분리하여 코드 구조 개선

--- 07-UIKit/DelegateDemo/DelegateDemo/MainViewController.swift
+++ 07-UIKit/DelegateDemo/DelegateDemo/MainViewController.swift
@@ -35,14 +35,5 @@ class MainViewController: UIViewController, CustomViewControllerDelegate {
     customVC.delegate = self
     present(customVC, animated: true)
   }
-
-  // MARK: - CustomViewControllerDelegate
-  func didTapButton(withText text: String) {
-    print("버튼이 탭되었습니다. 텍스트: \(text)")
-  }
-
-  func willAppear() {
-    print("CustomViewController가 나타날 예정입니다.")
-  }
 }
 
