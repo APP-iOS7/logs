91ee172bed3d81deb986a7c70310cef4893c4d50
JM Bae <jungman82@gmail.com>
2025-03-19 14:11:16 +0900

    데이터 관리 기능 추가 및 두 개의 뷰 컨트롤러에 데이터 전달 구현

--- 07-UIKit/TabDemo/TabDemo/SecondViewController.swift
+++ 07-UIKit/TabDemo/TabDemo/SecondViewController.swift
@@ -9,20 +9,57 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+  let dataLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 20)
+    label.textAlignment = .center
+    label.text = "데이터가 없습니다"
+    return label
+  }()
+  private var observer: NSObjectProtocol? // 옵저버 참조 저장
+
+
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.title = "Second"
+
+    setupLabel()
   }
 
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    NotificationCenter.default.removeObserver(self)
+  }
 
-  /*
-   // MARK: - Navigation
+  func setupLabel() {
+    view.addSubview(dataLabel)
+    dataLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      dataLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      dataLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
 
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
+    // NotificationCenter를 사용하여 데이터 변경 감지
+    observer = NotificationCenter.default.addObserver(
+      forName: DataManager.dataChangedNotification,
+      object: nil,
+      queue: .main) { [weak self] _ in
+        self?.updateLabel()
+      }
+  }
 
+  func updateLabel() {
+    if DataManager.shared.data.isEmpty {
+      dataLabel.text = "아직 데이터가 없습니다"
+    } else {
+      dataLabel.text = DataManager.shared.data
+    }
+  }
+
+  // 옵저버 해제
+  deinit {
+    if let observer = observer {
+      NotificationCenter.default.removeObserver(observer)
+    }
+  }
 }
