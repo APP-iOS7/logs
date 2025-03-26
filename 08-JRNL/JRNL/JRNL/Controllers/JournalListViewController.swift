971fb0db32f90a8e4189d13892b62eeea9b2e83a
JM Bae <jungman82@gmail.com>
2025-03-26 13:48:58 +0900

    UITextField 및 UITextView delegate 연결 및 구현

--- 08-JRNL/JRNL/JRNL/Controllers/JournalListViewController.swift
+++ 08-JRNL/JRNL/JRNL/Controllers/JournalListViewController.swift
@@ -8,28 +8,28 @@
 import UIKit
 
 class JournalListViewController: UIViewController {
-
+  
   @IBOutlet weak var tableView: UITableView!
-
+  
   var sampleJournalEntryData = SampleJournalEntryData()
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // 샘플 데이터 생성
     sampleJournalEntryData.createSampleJournalEntryData()
   }
-
+  
   @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
     print("unwindNewEntryCancel")
   }
-
+  
   @IBAction func unwindNewEntrySave(segue: UIStoryboardSegue) {
     if let sourceViewController = segue.source as? AddJournalEntryViewController,
        let newJournalEntry = sourceViewController.newJournalEntry {
-      sampleJournalEntryData.journalEntries.append(newJournalEntry)      
+      sampleJournalEntryData.journalEntries.append(newJournalEntry)
       tableView.reloadData()
     }
-
+    
   }
 }
 
@@ -37,20 +37,20 @@ extension JournalListViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return sampleJournalEntryData.journalEntries.count
   }
-
+  
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let journalCell = tableView.dequeueReusableCell(
       withIdentifier: "journalCell",
       for: indexPath
     ) as! JournalListTableViewCell
-
+    
     let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
     // 날짜, 제목, 사진 표시
     // 날짜는 "월 일, 년" 형식으로 표시
     journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.month().day().year())
     journalCell.titleLabel.text = journalEntry.entryTitle
     journalCell.photoImageView.image = journalEntry.photo
-
+    
     return journalCell
   }
 }
@@ -66,7 +66,7 @@ extension JournalListViewController: UITableViewDelegate {
       sampleJournalEntryData.journalEntries.remove(at: indexPath.row)
       // 테이블 전체 새로고침
       // tableView.reloadData()
-
+      
       // 테이블에서 해당 행만 삭제 ( 애니메이션 효과 포함 )
       tableView.deleteRows(at: [indexPath], with: .fade)
     }
