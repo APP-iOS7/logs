e5ad38078d3db7abae64f1f731a6b2a6f98b29b8
JM Bae <jungman82@gmail.com>
2025-04-03 09:15:17 +0900

    PowerfulSwiftUI 프로젝트 초기 설정 및 기본 UI 구성 추가 (LazyGrid 데모 추가)

---
+++ 09-Combine/PowerfulSwiftUI/PowerfulSwiftUI/PowerfulSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerfulSwiftUIApp.swift
+//  PowerfulSwiftUI
+//
+//  Created by Jungman Bae on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PowerfulSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
