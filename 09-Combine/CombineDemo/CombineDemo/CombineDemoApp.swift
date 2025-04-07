f6420311939b617e9258df124ecfb6eb645aee95
JM Bae <jungman82@gmail.com>
2025-04-07 09:36:56 +0900

    챕터 8: 컴바인 데모 프로젝트 생성

---
+++ 09-Combine/CombineDemo/CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by Jungman Bae on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
