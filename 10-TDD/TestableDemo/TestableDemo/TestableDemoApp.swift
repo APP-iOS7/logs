ece27ac2b58e0ec50bcf036a113f4a9bbf201ef5
JM Bae <jungman82@gmail.com>
2025-04-14 15:42:38 +0900

    프로젝트 초기 설정 및 FizzBuzz 기능 구현, 테스트 케이스 추가

---
+++ 10-TDD/TestableDemo/TestableDemo/TestableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestableDemoApp.swift
+//  TestableDemo
+//
+//  Created by Jungman Bae on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TestableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 10-TDD/TestableDemo/TestableDemoTests/TestableDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  TestableDemoTests.swift
+//  TestableDemoTests
+//
+//  Created by Jungman Bae on 4/14/25.
+//
+
+import XCTest
+@testable import TestableDemo
+
+final class TestableDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+      let result = fizzBuzz(3)
+      XCTAssertEqual(result, "Fizz")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
