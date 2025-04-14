14ba2703e72bb3a98011783d80064ffac0cdb424
JM Bae <jungman82@gmail.com>
2025-04-14 13:36:13 +0900

    INITIAL COMMIT: 10-TDD

---
+++ 10-TDD/HelloWorld.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+// 타겟 함수
+func fizzBuzz(_ number: Int) -> String {
+  return ""
+}
+
+// 테스트 함수
+func testFizzBuzz() {
+  print("FizzBuzz 테스트 시작")
+  print("FizzBuzz(1): \(fizzBuzz(1))")
+  print("FizzBuzz(2): \(fizzBuzz(2))")
+  print("FizzBuzz(3): \(fizzBuzz(3))")
+  print("FizzBuzz(5): \(fizzBuzz(5))")
+  print("FizzBuzz(15): \(fizzBuzz(15))")
+  print("FizzBuzz 테스트 완료")
+}
+
+testFizzBuzz()
