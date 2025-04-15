829ab811aef9d786bc5107ebb47ea65e6f8e482c
JM Bae <jungman82@gmail.com>
2025-04-15 11:35:06 +0900

    MenuGroupingTests.swift 파일 추가 및 기본 테스트 케이스 구현

---
+++ 10-TDD/Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -0,0 +1,33 @@
+//
+//  MenuGroupingTests.swift
+//  AlbertosTests
+//
+//  Created by Jungman Bae on 4/15/25.
+//
+
+import XCTest
+
+@testable import Albertos
+
+final class MenuGroupingTests: XCTestCase {
+
+    // 한 카테고리당 섹션이 하나씩 있어야 한다.
+    func testMenuWithManyCategoriesReturnsOneSectionPerCategory() {
+
+    }
+
+    // 카테고리가 하나인 경우 섹션도 하나여야 한다.
+    func testMenuWithOneCategoryReturnsOneSection() {}
+
+    // 메뉴가 비어있으면 섹션도 비어있어야 한다.
+    func testEmptyMenuReturnsEmptySections() {
+        // Arrange: 빈 메뉴
+        let menu = [MenuItem]()
+
+        // Act
+        let sections = groupMenuByCategory(menu)
+
+        // Assert
+        XCTAssertTrue(sections.isEmpty)
+    }
+}
