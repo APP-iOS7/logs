96d65caf70e0148500578ec6659a99af39c88450
JM Bae <jungman82@gmail.com>
2025-04-16 11:40:31 +0900

    MenuList 뷰모델에서 menuGrouping 매개변수를 제거하고, 메뉴를 가져오는 로직을 수정하여 초기 섹션을 빈 배열로 설정하도록 변경함. MenuListViewModelTests에서 빈 메뉴 리스트 발행 테스트 및 섹션 생성 테스트 추가.

--- 10-TDD/Albertos/Albertos/MenuList.swift
+++ 10-TDD/Albertos/Albertos/MenuList.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import Combine
 
 struct MenuList: View {
-  let viewModel: ViewModel
-  
+  @StateObject var viewModel: ViewModel
+
   var body: some View {
     List {
       ForEach(viewModel.sections) { section in
@@ -27,18 +28,26 @@ struct MenuList: View {
 extension MenuList {
   class ViewModel: ObservableObject {
     @Published private(set) var sections: [MenuSection]
-    
+
+    var cancellables = Set<AnyCancellable>()
+
     init(
       menuFetching: MenuFetching,
-      menuGrouping: @escaping ([MenuItem]) -> [MenuSection]) {
-        self.sections = menuGrouping([])
+      menuGrouping: @escaping ([MenuItem]) -> [MenuSection] = groupMenuByCategory) {
+        self.sections = []
+        menuFetching.fetchMenu()
+          .sink(
+            receiveCompletion: { _ in },
+            receiveValue: { [weak self] items in
+              self?.sections = menuGrouping(items)
+            })
+          .store(in: &cancellables)
       }
   }
 }
 
 #Preview {
   NavigationStack {
-    MenuList(viewModel: .init(menuFetching: MenuFetchingPlaceholder(),
-                              menuGrouping: groupMenuByCategory))
+    MenuList(viewModel: .init(menuFetching: MenuFetchingPlaceholder()))
   }
 }
