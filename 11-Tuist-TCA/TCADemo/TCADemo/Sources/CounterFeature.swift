304391db7a460c5da07d7cc820183455291ee89d
JM Bae <jungman82@gmail.com>
2025-04-24 11:30:25 +0900

    * 빌드오류 수정

--- 11-Tuist-TCA/TCADemo/TCADemo/Sources/CounterFeature.swift
+++ 11-Tuist-TCA/TCADemo/TCADemo/Sources/CounterFeature.swift
@@ -10,30 +10,30 @@ import ComposableArchitecture
 @Reducer
 struct CounterFeature {
   // MARK: - State
-  @ObservableState // SwiftUI 뷰에서 관찰 가능하도록 설정
-  struct State: Equatable { // 테스트 용이성을 위해 Equatable 채택
+  @ObservableState
+  struct State: Equatable {
     var count = 0
   }
-
+  
   // MARK: - Action
   enum Action {
     case incrementButtonTapped
     case decrementButtonTapped
   }
-
-  // MARK: - Reducer Body
+  
+  // MARK: - Reducer
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case .incrementButtonTapped:
         state.count += 1
         return .none
-
+        
       case .decrementButtonTapped:
         state.count -= 1
         return .none
-
+        
+      }
     }
   }
-
 }
