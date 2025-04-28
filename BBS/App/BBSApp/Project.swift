009bff469a22bb9459fbbe7c637cca9798e7ad50
JM Bae <jungman82@gmail.com>
2025-04-28 09:45:37 +0900

    기능 추가: BBSApp 구조체 및 AppDelegate 구현, Firebase 설정 추가

--- BBS/App/BBSApp/Project.swift
+++ BBS/App/BBSApp/Project.swift
@@ -1,34 +1,43 @@
 import ProjectDescription
 
+let bundleId = "kr.co.codegrove.BBSApp"
+
 let project = Project(
-    name: "BBSApp",
-    targets: [
-        .target(
-            name: "BBSApp",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "io.tuist.BBSApp",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            sources: ["BBSApp/Sources/**"],
-            resources: ["BBSApp/Resources/**"],
-            dependencies: []
-        ),
-        .target(
-            name: "BBSAppTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "io.tuist.BBSAppTests",
-            infoPlist: .default,
-            sources: ["BBSApp/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "BBSApp")]
-        ),
-    ]
+  name: "BBSApp",
+  targets: [
+    .target(
+      name: "BBSApp",
+      destinations: .iOS,
+      product: .app,
+      bundleId: bundleId,
+      infoPlist: .extendingDefault(
+        with: [
+          "UILaunchScreen": [
+            "UIColorName": "",
+            "UIImageName": "",
+          ],
+        ]
+      ),
+      sources: ["BBSApp/Sources/**"],
+      resources: ["BBSApp/Resources/**"],
+      dependencies: [
+        .external(name: "FirebaseAnalytics"),
+        .external(name: "FirebaseCrashlytics"),
+        .external(name: "FirebaseCore"),
+        .external(name: "FirebaseFirestore"),
+        .external(name: "FirebaseAuth"),
+        .external(name: "FirebaseStorage"),
+      ]
+    ),
+    .target(
+      name: "BBSAppTests",
+      destinations: .iOS,
+      product: .unitTests,
+      bundleId: "\(bundleId).Tests",
+      infoPlist: .default,
+      sources: ["BBSApp/Tests/**"],
+      resources: [],
+      dependencies: [.target(name: "BBSApp")]
+    ),
+  ]
 )
